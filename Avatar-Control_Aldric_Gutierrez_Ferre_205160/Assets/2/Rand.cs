a53d5ad061b85dee2cb9c6168eebaaeb1d6f102e
AldricGF <aldricgutierrezferreid@gmail.com>
2020-06-11 10:49:17 +0200

    IS-P5 Avatar Control Updated

---
+++ Avatar-Control_Aldric_Gutierrez_Ferre_205160/Assets/2/Rand.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Rand : MonoBehaviour
+{
+    AudioSource source;
+    public AudioClip audio1;
+    public AudioClip audio2;
+    public AudioClip audio3;
+    public AudioClip audio4;
+
+    int id;
+
+    void Start()
+    {
+    	AudioSource source = GetComponent<AudioSource>();
+
+        System.Random rnd = new System.Random();
+		id = rnd.Next(1,5);
+
+		if(id == 1){
+			source.clip = audio1;
+			source.Play(0);
+		}
+		if(id == 2){
+			source.clip = audio2;
+			source.Play(0);
+		}
+		if(id == 3){
+			source.clip = audio3;
+			source.Play(0);
+		}
+		if(id == 4){
+			source.clip = audio4;
+			source.Play(0);
+		}
+
+        Debug.Log("started");
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Avatar-Control_Aldric_Gutierrez_Ferre_205160/README.md
@@ -0,0 +1,20 @@
+For this delivery, a modified avatar is introduced into our final project.
+
+In this work-in-progress, the user can select four different zones using their two wrists and two feet.
+
+The avatar is modified and positioned such that it can easily reach for these zones.
+
+On contact, the avatar goes through the sphere gameObjects. But if we make the spheres solid, the avatar will also kinematically interact with them.
+
+
+# PoseNetOSC-Unity
+
+This project is based on these other projects: 
+
+PoseNet to OSC adaptation
+https://github.com/tommymitch/posenetosc 
+
+extOSC unity package
+https://assetstore.unity.com/packages/tools/input-management/extosc-open-sound-control-72005
+
+Follow the instructions to run PoseNet OSC and then run the Unity project
